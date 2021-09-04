9cfe2a574cf0efc6cd6ecfd05dbdee2ca7548578
Ana Oliveira <vianello.ana@gmail.com>
2021-09-04 18:11:53 -0300

    Seeds: authors and articles + corrections on migrations

---
+++ src/database/seeds/003_articles.js
@@ -0,0 +1,35 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('articles').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('articles').insert([
+        {
+          author_id: '4',
+          category: 'xxxxxx',
+          title: 'Very good title',
+          summary: 'very interesting article',
+          first_paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        },
+        {
+          author_id: '5',
+          category: 'yyyyyy',
+          title: 'Really good title',
+          summary: 'wow',
+          first_paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        },
+        {
+          author_id: '6',
+          category: 'zzzzzz',
+          title: 'Honest title',
+          summary: 'Stunning',
+          first_paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        }
+
+      ]);
+    });
+};
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,16 @@ const knex = require('./database')
 const app = express.Router();
 const UserController = require('./controllers/UserController')
 
-app.get('/users', UserController.index)
-app.post('/users', UserController.singUp)
+// Routes users
+app
+.get('/users', UserController.index)
+.post('/users', UserController.singUp)
+.put('/users/:id', UserController.update)
+.delete('/users/:id', UserController.delete)
 
-app.put('/users/:id', UserController.update)
+// Routes authors
+
+
 
 
 module.exports = app
